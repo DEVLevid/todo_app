32fadd94c07e31fc1c64a73f42806cf45964e86f
DEVLevid <levidlima80@gmail.com>
2024-09-20 11:52:19 -0300

    refactor: Refactoring the path file.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
-import Header from "@/components/Header/Header";
+
+import Header from "../components/Header/Header";
 import style from "./page.module.scss";
-import Tasks from "@/components/Tasks/Tasks";
+import Tasks from "../components/Tasks/Tasks";
 
 export default function Home() {
   return (
